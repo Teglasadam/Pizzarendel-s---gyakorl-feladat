74deb68ae11fef9e858e831d8a7931dc741a4971
Téglás Ádám <ad.teglas@gmail.com>
2024-01-04 17:21:52 +0100

    Rendelések létrehozva

---
+++ app.js
@@ -0,0 +1,52 @@
+const orders = [
+	{
+		id: 1,
+		pizza: "Hawaii",
+		extra: "Kukrica",
+		ital: "Pepsi zero",
+		ar: 1500,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+	{
+		id: 2,
+		pizza: "Húsimádó",
+		extra: "Sonka",
+		ital: "Cola zero",
+		ar: 1600,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+	{
+		id: 3,
+		pizza: "Malacos",
+		extra: "Bacon",
+		ital: "Ásványvíz",
+		ar: 1700,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+	{
+		id: 4,
+		pizza: "SajtosMajtos",
+		extra: "Parmezán",
+		ital: "Nincs",
+		ar: 1800,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+	{
+		id: 5,
+		pizza: "Édes-Királynő",
+		extra: "Csoki",
+		ital: "Shake'n'Espresso",
+		ar: 1900,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+	{
+		id: 6,
+		pizza: "Tüzes",
+		extra: "Pepperóni",
+		ital: "Chilipálinka",
+		ar: 2000,
+		kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	},
+];
+
+
